a28d51817a5419f7de49b2b532e37d10efc220a0
Ren <piyushrngrup@gmail.com>
2016-05-07 00:10:41 +0530

    Adding files in git

---
+++ Assets/ParticleEffects/selfDestroy.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class selfDestroy : MonoBehaviour {
+    private ParticleSystem particleSystem;
+
+    void Start() {
+        particleSystem = this.GetComponent<ParticleSystem>();
+    }
+
+    void Update() {
+        if (!particleSystem.isPlaying)
+            Destroy(this.gameObject);
+    }
+}
