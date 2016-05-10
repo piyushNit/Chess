b546bda862fad484ad1b1f1604c537206a2b9041
Ren <piyushrngrup@gmail.com>
2016-05-10 22:54:52 +0530

    New HUDController script to handle HUD button clicks.

---
+++ Assets/Scripts/HUD/HUDController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUDController : MonoBehaviour {
+
+    GameplayScene gameplayScene;
+
+    public void ResetTheGame() {
+        GameStateManager.instance.ResetTheGame();
+    }
+
+    public void UndoTheSteps() {
+        if (gameplayScene == null)
+            SetGamePlaySceneVar();
+        gameplayScene.UndoTheStep();
+    }
+
+    void SetGamePlaySceneVar() {
+        gameplayScene = GameObject.FindObjectOfType<GameplayScene>();
+    }
+}
