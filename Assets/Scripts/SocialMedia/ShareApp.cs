248199d9edf34fe31be82b73ca7b21d8e58354d5
Ren <piyushrngrup@gmail.com>
2016-05-15 00:14:36 +0530

    Add app share support.

---
+++ Assets/Scripts/SocialMedia/ShareApp.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShareApp : MonoBehaviour {
+    string subject = "WORD-O-MAZE";
+    string body = "PLAY THIS AWESOME GAME. GET IT ON THE PLAYSTORE AT LINK";
+
+    public void shareText() {
+        //execute the below lines if being run on a Android device
+    #if UNITY_ANDROID
+        //Refernece of AndroidJavaClass class for intent
+        AndroidJavaClass intentClass = new AndroidJavaClass("android.content.Intent");
+        //Refernece of AndroidJavaObject class for intent
+        AndroidJavaObject intentObject = new AndroidJavaObject("android.content.Intent");
+        //call setAction method of the Intent object created
+        intentObject.Call<AndroidJavaObject>("setAction", intentClass.GetStatic<string>("ACTION_SEND"));
+        //set the type of sharing that is happening
+        intentObject.Call<AndroidJavaObject>("setType", "text/plain");
+        //add data to be passed to the other activity i.e., the data to be sent
+        intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_SUBJECT"), subject);
+        intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_TEXT"), body);
+        //get the current activity
+        AndroidJavaClass unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject>("currentActivity");
+        //start the activity by sending the intent data
+        currentActivity.Call("startActivity", intentObject);
+    #endif
+    }
+
+    public void OnRateButtonClick()
+    {
+        #if UNITY_ANDROID
+                Application.OpenURL("https://www.facebook.com/RenGames-764437100281341/?ref=aymt_homepage_panel");
+        #elif UNITY_IPHONE
+             Application.OpenURL("itms-apps://itunes.apple.com/app/idYOUR_APP_ID");
+        #endif
+    }
+
+}
