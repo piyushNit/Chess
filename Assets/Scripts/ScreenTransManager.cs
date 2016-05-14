3ea04f5b38cb22f83add6cd7f5434f62c8ea3c25
Ren <piyushrngrup@gmail.com>
2016-05-15 00:15:57 +0530

    Add class to support screen transition.

---
+++ Assets/Scripts/ScreenTransManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ScreenTransManager : MonoBehaviour {
+    [SerializeField] private string mainMenuScene;
+
+    public void GoToMainMenuScene() {
+        SceneManager.LoadScene(mainMenuScene);
+    }
+
+    public void ResetTheGame() {
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
+    }
+}
