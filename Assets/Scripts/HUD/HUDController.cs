0e6ed4b1841470eb08b4302d0b3d89ae842bd6c9
Ren <piyushrngrup@gmail.com>
2016-05-15 00:16:31 +0530

    Add Game over dialog box.

--- Assets/Scripts/HUD/HUDController.cs
+++ Assets/Scripts/HUD/HUDController.cs
@@ -4,9 +4,25 @@ using System.Collections;
 public class HUDController : MonoBehaviour {
 
     GameplayScene gameplayScene;
+    [SerializeField] private GameObject gameOverCanvas;
+    [SerializeField] private float gameOverCanvasAnimationTime = 0.5f;
+    private Vector3 gameOverCanvasTargetPos;
+    private Vector3 gameOverCavasDefaultPos;
+
+    private ScreenTransManager screenTransManager;
+
+    void Start() {
+        InitMemberVars();
+    }
+
+    private void InitMemberVars() {
+        gameOverCavasDefaultPos = gameOverCanvas.transform.position;
+        gameOverCanvasTargetPos = new Vector3(Screen.width / 2, Screen.height / 2, 0);
+        screenTransManager = GameObject.FindObjectOfType<ScreenTransManager>();
+    }
 
     public void ResetTheGame() {
-        GameStateManager.instance.ResetTheGame();
+        screenTransManager.ResetTheGame();
     }
 
     public void UndoTheSteps() {
@@ -17,5 +33,13 @@ public class HUDController : MonoBehaviour {
 
     void SetGamePlaySceneVar() {
         gameplayScene = GameObject.FindObjectOfType<GameplayScene>();
+    }
+
+    public void ShowGameOver() {
+        iTween.MoveTo(gameOverCanvas, gameOverCanvasTargetPos, gameOverCanvasAnimationTime);
+    }
+
+    public void GoToMainMenu() {
+        screenTransManager.GoToMainMenuScene();
     }
 }
